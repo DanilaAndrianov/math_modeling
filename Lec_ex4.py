b47520908a1c02fdc77b6e99ac7753c9492a2f95
DanilaAndrianov <71896687+DanilaAndrianov@users.noreply.github.com>
2020-09-28 17:38:55 +0200

    Listen lesson
    I listened lessons

---
+++ Lec_ex4.py
@@ -0,0 +1,19 @@
+a=3
+b=4
+c=5
+if a>4 and b==2:
+    print('Good')
+elif b>3 or c==5:
+    print('best')
+else:
+    print('bad')
+    
+    
+if a>=3 or b==5:
+    print('good')
+if c==-1 or b>4:
+    print('best')
+else:
+    print('bad')
+    
+
---
+++ Lec_ex9.py
@@ -0,0 +1,12 @@
+for i in 'hello' :
+    if i=='o' :
+        break
+    print(i*2,end='')
+
+    
+    
+    
+    
+    
+    
+
