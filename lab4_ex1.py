8dd24b28e43a36ffd263feab6395957e339c256c
DanilaAndrianov <71896687+DanilaAndrianov@users.noreply.github.com>
2020-10-26 18:44:40 +0200

    функция
    одоиывмолдьтумарлит23кищз\лт\ фыоьицмолдзийкцущзвуимЦШЩОЦЮВ ОЛЦСМОЮ ЙЦМ ЙЧЛСРЙУАРЛЦО2Р4ЕТ2ЯСЮ \ТМЛШЮРН

---
+++ lab4_ex1.py
@@ -0,0 +1,9 @@
+def срзнач (a):
+    x=sum(a)/len(a)
+    return x
+a=[1,3,5]
+print(срзнач(a))
+
+ 
+       
+    
---
+++ lab4_ex2.py
@@ -0,0 +1,6 @@
+import numpy as np
+def koko (x):
+    x = np.prod(x)
+    return x
+a=np.array([1,3,5])
+print(koko(a))
